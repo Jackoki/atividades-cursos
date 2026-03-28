de25ad5d270f8f8cf4a31f7a2a58f3af9087331d
Jackoki <kenji.inoue@hotmail.com>
2026-03-27 23:40:29 -0300

    Implementação do Chain of Responsability para realização da validações para o AdocaoService

--- curso-java-refatoracao-api/src/main/java/br/com/alura/adopet/api/service/AdocaoService.java
+++ curso-java-refatoracao-api/src/main/java/br/com/alura/adopet/api/service/AdocaoService.java
@@ -18,6 +18,7 @@ import br.com.alura.adopet.api.model.StatusAdocao;
 import br.com.alura.adopet.api.repository.AdocaoRepository;
 import br.com.alura.adopet.api.repository.PetRepository;
 import br.com.alura.adopet.api.repository.TutorRepository;
+import br.com.alura.adopet.api.validacoes.ValidacaoSolicitacaoAdocao;
 
 @Service
 public class AdocaoService {
@@ -34,11 +35,14 @@ public class AdocaoService {
     @Autowired
     private EmailService emailService;
 
+    @Autowired
+    private List<ValidacaoSolicitacaoAdocao> validacoes;
+
     public void solicitar(SolicitacaoAdocaoDto dto) {
         Pet pet = petRepository.getReferenceById(dto.idPet());
         Tutor tutor = tutorRepository.getReferenceById(dto.idTutor());
 
-        
+        validacoes.forEach(v -> v.validar(dto));
 
         Adocao adocao = new Adocao();
         adocao.setData(LocalDateTime.now());
