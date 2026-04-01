335cc64968e44c99e90c3653d592ec6e800f3d55
Jackoki <kenji.inoue@hotmail.com>
2026-03-31 23:44:56 -0300

    Finalização do curso realizando a transformação do retorno para JSON

---
+++ curso-java-reflection/src/main/java/br/com/alura/refl/ObjectToJsonTester.java
@@ -0,0 +1,11 @@
+package br.com.alura.refl;
+
+import br.com.alura.Pessoa;
+
+public class ObjectToJsonTester {
+    public static void main(String... x) {
+        Pessoa pessoa = new Pessoa(1, "João", "12345");
+        ObjectToJson objectToJson = new ObjectToJson();
+        System.out.println(objectToJson.transform(pessoa));
+    }
+}
