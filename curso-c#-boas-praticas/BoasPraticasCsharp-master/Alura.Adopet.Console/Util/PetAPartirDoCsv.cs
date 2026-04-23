9d28605eb87137a8febd51494405854ff2f6474d
Jackoki <kenji.inoue@hotmail.com>
2026-04-23 20:33:53 -0300

    Inserção de testes automatizados no projeto

---
+++ curso-c#-boas-praticas/BoasPraticasCsharp-master/Alura.Adopet.Console/Util/PetAPartirDoCsv.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Alura.Adopet.Console.Modelos;
+
+namespace Alura.Adopet.Console.Util
+{
+    public class PetAPartirDoCsv
+    {
+        public Pet ConverteDoTexto(string linha)
+        {
+            string[]? propriedades = linha.Split(';');
+
+            Pet pet = new Pet(Guid.Parse(propriedades[0]), propriedades[1], int.Parse(propriedades[2]) == 1 ? TipoPet.Gato : TipoPet.Cachorro);
+
+            return pet;
+        }
+    }
+}
---
+++ curso-c#-boas-praticas/BoasPraticasCsharp-master/Alura.Adopet.Testes/PetAPartirDoCsvTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Alura.Adopet.Console.Modelos;
+using Alura.Adopet.Console.Util;
+
+namespace Alura.Adopet.Testes
+{
+    public class PetAPartirDoCsvTest
+    {
+        [Fact]
+        public void QuandoStringForValidaDeveRetornarUmPet()
+        {
+            string linha = "456b24f4-19e2-4423-845d-4a80e8854a41;Lima Limão;1";
+            var conversor = new PetAPartirDoCsv();
+
+            Pet pet = conversor.ConverteDoTexto(linha);
+
+            Assert.NotNull(pet);
+        }
+    }
+}
